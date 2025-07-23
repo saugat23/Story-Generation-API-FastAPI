620b7db3095ca07f8fab05c0046b41f24d3a3359
saugat23 <saugatbhandari8@gmail.com>
2025-07-23 21:11:16 +0545

    fastapi config

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,22 @@
-def main():
-    print("Hello from backend!")
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
+app = FastAPI(
+    title="Adventure/Story game API",
+    description="API to create stories using OpenAI LLM",
+    version="0.1.0",
+    docs_url="/docs",
+    redoc_url="/redoc",
+)
 
-if __name__ == "__main__":
-    main()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+if __name__=="__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
